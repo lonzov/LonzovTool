58a1822c8ff38ecd44932531b02e4a2337483502
lonzov <mcss6699@qq.com>
2026-06-17 20:54:27 +0800

    feat(/down): 添加 HOSDLA工具箱 的下载页

--- src/data/downloads/index.js
+++ src/data/downloads/index.js
@@ -2,12 +2,14 @@
 export { default as zhilingYinfuhe } from './zhiling-yinfuhe.json'
 export { default as mcYufabao } from './mc-yufabao.json'
 export { default as AIEXFMBE } from './AIEX-FMBE.json'
+export { default as HOSDLA } from './HOSDLA-toolbox.json'
 
 // slug → 模块映射
 const moduleMap = {
   'zhiling-yinfuhe': () => import('./zhiling-yinfuhe.json'),
   'mc-yufabao': () => import('./mc-yufabao.json'),
   'AIEX-FMBE': () => import('./AIEX-FMBE.json'),
+  'HOSDLA-toolbox': () => import('./HOSDLA-toolbox.json')
 }
 
 export function getDownloadConfig(slug) {
